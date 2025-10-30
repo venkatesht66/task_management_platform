3f25a925c08e9f3d2848b131b517e342e2132413
venkateshfswb <tammisettyvenkatesh@Venkys-MacBook-Air.local>
2025-10-30 08:53:11 +0530

    .env modified

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI;
+  const uri = process.env.MONGO_URI || 'mongodb+srv://venkatesht6:venky457146@clusterelection.hewacgw.mongodb.net/?retryWrites=true&w=majority&appName=ClusterElection';
   try {
     await mongoose.connect(uri, { dbName: 'task_manager' });
     console.log('MongoDB connected');
--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const SECRET = process.env.JWT_SECRET;
+const SECRET = process.env.JWT_SECRET || 'supersecret';
 
 function signToken(payload, expiresIn = '7d') {
   return jwt.sign(payload, SECRET, { expiresIn });
